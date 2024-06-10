4bc983265f82f8e4e1d9f035a2cc72a1df824a5e
BalajiSriraman <balajisrdev@gmail.com>
2024-06-11 00:28:45 +0530

    chore: Update dependencies and add Rocket.toml configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
-#[tokio::main]
-async fn main() {
-    let routes = routes::routes();
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::Surreal;
 
-    println!("Server started at http://localhost:8000");
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+#[macro_use] extern crate rocket;
+
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+#[get("/ping")]
+async fn index() -> &'static str {
+  let db = Surreal::new::<Ws>("0.0.0.0:8000").await;
+
+  
+    // if db is None, then the connection failed
+    if db.is_ok() {
+    db.signin(Root {
+    username: "root" ,
+    password: "root",
+    })
+    .await;
+
+}
+"Jeff"
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, world])
 }
