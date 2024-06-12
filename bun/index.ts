cbe6b7bdfeb32bb0331c726a9ad3cdc7af32baeb
BalajiSriraman <balajisrdev@gmail.com>
2024-06-12 20:51:32 +0530

    chore: Update database file path and add dependencies

--- bun/index.ts
+++ bun/index.ts
@@ -1,11 +1,26 @@
 import { Elysia } from 'elysia'
-import { root } from './main'
+import { read, write } from './main'
 import consola from 'consola'
+import { Surreal } from 'surrealdb.js';
+
+const db = new Surreal();
 
 
+await db.connect('http://127.0.0.1:8000/rpc', {
+  // Set the namespace and database for the connection
+  namespace: 'test',
+  database: 'test',
+
+  // Set the authentication details for the connection
+  auth: {
+    username: 'root',
+    password: 'root',
+  },
+});
 
 new Elysia()
-  .get('/', async () => await root())
+  .get('/', async () => await read(db))
+  .get('/write', async () => await write(db))
   .listen(3000)
 
 consola.box("Server running on port 3000")
