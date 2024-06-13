48257f972be41c8a9f8a1e5a20c9dae90510cfb5
BalajiSriraman <balajisrdev@gmail.com>
2024-06-13 23:09:56 +0530

    chore: Remove bun-related files and update start.sh script

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
 surreal start --log trace --user root --pass root file:mydatabase.db
+
+surreal start --log trace --auth memory
+
+surreal import --conn http://localhost:8000 --user root --pass root --ns test --db test surreal-deal-v2.surql
+
